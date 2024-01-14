627de0be801530e12bf7547a7724e5a218623c1e
Zain Khan <zainkhanjune@gmail.com>
2024-01-14 21:02:49 +0530

    Added Day 1 part 1 of advent of code

---
+++ day1/01.py
@@ -0,0 +1,24 @@
+def get_sum(text):
+	num = 0
+	for c in text:
+		if '0' <= c <= '9':
+			num += ord(c) - ord('0')
+			break
+	num *= 10
+	for c in text[::-1]:
+		if '0' <= c <= '9':
+			num += ord(c) - ord('0')
+			break
+	return num
+
+
+
+if __name__ == "__main__":
+	total_sum = 0
+	with open("01.txt") as file:
+		text = file.readlines()
+		for line in text:
+			sum = get_sum(line)
+			print(sum)
+			total_sum += sum
+	print(total_sum)
